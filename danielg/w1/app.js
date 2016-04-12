2b53f87fe5fff14e7622fc4d3779dd088e4b2f9d
Daniel Gofman <d.gofman@web.de>
2016-04-12 17:49:40 +0200

    Aufgabe 3 erledigt + sortierte json datei hinzugefügt

--- danielg/w1/app.js
+++ danielg/w1/app.js
@@ -9,11 +9,24 @@ fs.readFile(__dirname+filename, 'utf8', function(err, data) {
     }
     data = JSON.parse(data);
     
+    data.wolkenkratzer.sort(function (a, b) {
+      if (a.hoehe > b.hoehe) {
+        return 1;
+      }
+      if (a.hoehe < b.hoehe) {
+        return -1;
+      }
+      // a must be equal to b
+      return 0;
+    });
+    
+    fs.writeFileSync(__dirname+"/wolkenkratzer_sortiert.json",JSON.stringify(data));
+    
     for(var i = 0; i < data.wolkenkratzer.length; i++) {
         console.log("Name: "+chalk.blue(data.wolkenkratzer[i].name));
         console.log("Stadt: "+chalk.green(data.wolkenkratzer[i].stadt));
         console.log("Höhe: "+chalk.red(data.wolkenkratzer[i].hoehe));
         console.log("--------------------");
     }
-
+    
 });
