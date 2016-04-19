c405fea14a5aea47e3d462539a36d96735bd12f5
Daniel <Daniel@daniel.de>
2016-04-19 16:44:29 +0200

    SOLUTION Task 3

--- Dansel94/Workshop/Workshop_1/wolkenkratzer1.js
+++ Dansel94/Workshop/Workshop_1/wolkenkratzer1.js
@@ -6,7 +6,22 @@ var file = fs.readFile(__dirname+"/wolkenkratzer.json",'utf-8', function (err, d
 		console.log(err);
 		return;
 	} else {
+		//data in einen JSON String wandeln
 		data = JSON.parse(data);
+		
+		//Automatisches sortieren ASC
+		data.wolkenkratzer.sort(function (a,b) {
+			if (a.hoehe < b.hoehe) {
+				return 1;
+			}
+			if (a.hoehe > b.hoehe) {
+				return -1;
+			}
+			return 0;
+		});
+		//strinify als String in Datei
+		fs.writeFileSync(__dirname+"/wolkenkratzer_sortiert.json", JSON.stringify(data));
+		
 		for (var i=0; i<data.wolkenkratzer.length; i++) {
 			console.log("Name: " + chalk.blue(data.wolkenkratzer[i].name));
 			console.log("Stadt: " + chalk.red(data.wolkenkratzer[i].stadt));
