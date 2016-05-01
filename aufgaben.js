1b88075200dd7e438d4edfb87d0845cc1ae97a31
Nico Ferdinand <jackridder@hotmail.de>
2016-05-01 18:17:13 +0200

    Aufgabe 3 hinzugefügt und sortierte JSON Datei erstellen lassen

--- aufgaben.js
+++ aufgaben.js
@@ -8,7 +8,20 @@ fs.readFile('wolkenkratzer.json', 'utf-8', function(err, data) {
 	else{
 		//Aus data wird ein JSON String
 		data = JSON.parse(data);
-		//console.log(data);
+		data.wolkenkratzer.sort(function(a,b){
+			if(a.hoehe > b.hoehe){
+				return 1;
+			}
+			if(a.hoehe < b.hoehe){
+				return -1;
+			}
+			//Falls a = b ist
+			return 0;
+		});
+		
+		fs.writeFileSync("sortiert.json", JSON.stringify(data));
+		
+		
 		for (var i = 0; i < data.wolkenkratzer.length; i++) {
 			console.log("Name: " + chalk.green(data.wolkenkratzer[i].name));
 			console.log("Stadt: " + chalk.yellow(data.wolkenkratzer[i].stadt));
