cd2181b73430acab8a6e471bb5417065e70ae58a
Nico Ferdinand <jackridder@hotmail.de>
2016-05-01 17:50:39 +0200

    Chalk Variable erstellt und Name, Stadt und Hoehe eingefärbt

--- aufgaben.js
+++ aufgaben.js
@@ -1,3 +1,5 @@
+var chalk = require('chalk');
+
 var fs = require('fs');
 fs.readFile('wolkenkratzer.json', 'utf-8', function(err, data) {
 	if(err){
@@ -8,9 +10,9 @@ fs.readFile('wolkenkratzer.json', 'utf-8', function(err, data) {
 		data = JSON.parse(data);
 		//console.log(data);
 		for (var i = 0; i < data.wolkenkratzer.length; i++) {
-			console.log("Name: " + data.wolkenkratzer[i].name);
-			console.log("Stadt: " + data.wolkenkratzer[i].stadt);
-			console.log("Hoehe: " + data.wolkenkratzer[i].hoehe + "m");
+			console.log("Name: " + chalk.green(data.wolkenkratzer[i].name));
+			console.log("Stadt: " + chalk.yellow(data.wolkenkratzer[i].stadt));
+			console.log("Hoehe: " + chalk.blue(data.wolkenkratzer[i].hoehe + "m"));
 			console.log("--------------------");
 		}
 	}
