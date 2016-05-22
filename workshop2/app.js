9b29429da1de5aeca3103359f4d6100af5a0a092
Daniel <Daniel@daniel.de>
2016-05-22 17:50:56 +0200

    Workshop 2 hinzugefuegt

--- workshop2/app.js
+++ workshop2/app.js
@@ -14,3 +14,31 @@ app.post('/dice',jsonParser,function(req,res){
     var randNumber = Math.round(Math.random() * (6 - 1) + 1);
     res.end(randNumber.toString());
 });
+
+app.get('/gamefield',function (req,res) {
+        res.format({
+         'image/jpeg':function(){
+            res.sendFile('/gamefield.jpg');
+        }   
+     })    
+    res.writeHead(200, "OK");
+    res.end();
+});
+
+app.get('.',function (req,res) {
+        res.format({
+         'text/html':function(){
+        }   
+     })    
+    res.writeHead(200, "OK");
+    res.sendFile(path.join(__dirname+'./index.html'));
+    //res.sendFile('./index.html', function (err){
+    //   if(err) {
+    //        console.log(err);
+    //    }
+    //    else{
+    //        console.log("Datei geschickt!");
+    //    }
+    //    res.end();
+    //});
+});
