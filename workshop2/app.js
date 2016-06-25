8a59fc89a87484feda9f41a7b40385382f479b72
Nico Ferdinand <jackridder@hotmail.de>
2016-06-25 16:55:20 +0200

    Modified

--- workshop2/app.js
+++ workshop2/app.js
@@ -142,3 +142,49 @@ app.get('/',function (req,res) {
         res.end();
     });
 });
+
+
+
+var app = require('http');
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+
+var optionsGET = {
+  host: 'localhost',
+  port: 3000,
+  path: 'leer',
+  method: 'GET',
+  headers: {
+    accept: 'application/json'
+  }
+}
+
+var optionsPOST = {
+  host: 'localhost',
+  port: 3000,
+  path: 'leer',
+  method: 'POST',
+  headers: {
+    accept: 'application/json'
+  }
+}
+
+var clientID;
+
+optionsPOST.path = '/Spielfigur';
+var postSpielfigur = http.request(optionsPOST, function(res){
+  console.log("Spieler hat sich verbunden");
+  res.on('data',function(){
+    ClientID = jsonParser(data);
+    console.log(res);
+  });
+});
+postSpielfigur.end();
+
+options.path = '/gamefield';
+setTimeout(http.request(optionsGET,function(res){
+  res.on('data',function()){
+    console.log(res);
+  }
+}), 10000);
+end();
