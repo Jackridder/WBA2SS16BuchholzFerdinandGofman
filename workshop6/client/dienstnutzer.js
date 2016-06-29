dc3f935aa3ed04229116cdca2a6b50441381b190
Daniel Gofman <d.gofman@web.de>
2016-06-29 16:28:38 +0200

    gamefield and players added in client

--- workshop6/client/dienstnutzer.js
+++ workshop6/client/dienstnutzer.js
@@ -31,11 +31,9 @@ function startGame(){
 }
 
 io.on('connection',function(socket){
-  console.log("socket verbunden");
-
+  console.log("IO: Socket verbunden");
   socket.on('join',function(socket){
     console.log("Player joined");
-    io.emit('players',{data:allClients.length});
     newPlayer();
     if(allClients.length==4)
       startGame();
@@ -47,11 +45,22 @@ io.on('connection',function(socket){
 });
 
 io.sockets.on('connection', function(socket) {
+  console.log("SOCKETS: new Socket");
    allClients.push(socket);
    io.emit('players',{data:allClients.length});
-
+   socket.on('dice',function (){
+     console.log("player diced.");
+     request('http://localhost:3000/dice', function (error, response, body) {
+       if (!error && response.statusCode == 200) {
+         console.log("dice result: "+body);
+         io.emit('diced',{data:body});
+       }
+     });
+   });
    socket.on('disconnect', function() {
       console.log('Player left');
+
+      socket.emit('left',{data:allClients.length});
 
       var i = allClients.indexOf(socket);
       allClients.splice(i, 1);
@@ -65,6 +74,36 @@ io.sockets.on('connection', function(socket) {
 app.set('view engine', 'ejs');
 app.get('/',function (req,res) {
     res.render('index');
+});
+
+app.get('/gamefield',function (req,res) {
+  res.sendFile(__dirname+'/gamefield/gamefield.jpg', function (err){
+     if(err) {
+          console.log(err);
+          res.status(404).end("Datei nich gefunden");
+      }
+      else{
+          console.log("Datei geschickt!");
+      }
+      res.end();
+  });
+});
+
+app.get('/spielfigur/:picid',function (req,res) {
+    var filename = req.params.picid+".png";
+    res.sendFile(__dirname+'/spielfigur/'+filename, function (err){
+       if(err) {
+            console.error("SendFile error:", err, " (status: " + err.status + ")");
+            //console.log(err);
+            if (err.status) {
+              res.status(err.status).end();
+            }
+        }
+        else{
+            console.log("Datei geschickt!");
+        }
+        res.end();
+    });
 });
 
 function newPlayer(){
@@ -85,7 +124,6 @@ function getPlayerCount(){
       players = body;
     }
   });
-
 }
 
 function abortGame(){
