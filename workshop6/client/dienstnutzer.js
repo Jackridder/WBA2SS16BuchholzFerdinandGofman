66a0f8a91e79dd67a850b1f7cc3393df6881a7fe
Daniel Gofman <d.gofman@web.de>
2016-06-29 14:09:48 +0200

    cleanup

--- workshop6/client/dienstnutzer.js
+++ workshop6/client/dienstnutzer.js
@@ -3,7 +3,6 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var request = require('request');
 var allClients = [];
-//var app = express();
 
 server.listen(3001);
 console.log("Dienstnutzer auf Port 3001");
@@ -27,12 +26,8 @@ var optionsPOST = {
   }
 }
 
-//var io = require('socket.io').listen(3000);
-//var socketDienstnutzer = require('socket.io-client')('http://localhost:3000');
-//var socketDienstnutzer = io.connect('http://localhost');
 function startGame(){
   console.log("4 Spieler verbunden. Starte spiel");
-
 }
 
 io.on('connection',function(socket){
@@ -48,9 +43,6 @@ io.on('connection',function(socket){
   socket.on('abort',function(socket){
     console.log("Player aborted");
     newPlayer();
-
-    if(allClients.length==4)
-      abortGame();
   });
 });
 
@@ -63,30 +55,19 @@ io.sockets.on('connection', function(socket) {
 
       var i = allClients.indexOf(socket);
       allClients.splice(i, 1);
+
+      if(allClients.length==0){
+        abortGame();
+      }
    });
 });
 
-
-/*
-app.listen(3001,function(){
-});*/
 app.set('view engine', 'ejs');
 app.get('/',function (req,res) {
     res.render('index');
 });
 
 function newPlayer(){
-  /*
-  optionsPOST.path = '/spielfigur';
-
-  var postSpielfigur = server.request(optionsPOST, function(res){
-    console.log("Spieler hat sich verbunden");
-    res.on('data', function(chunk){
-        data = chunk;
-    });
-  });
-  postSpielfigur.end();
-  */
   data = false;
   request.post('http://localhost:3000/spielfigur', function (error, response, body) {
     if (!error && response.statusCode == 200) {
@@ -98,15 +79,6 @@ function newPlayer(){
 }
 
 function getPlayerCount(){
-  /*optionsGET.path = '/spielfigur';
-  var data = "00";
-  var getPlayerCount = server.request(optionsGET, function(res){
-    console.log("Hole Spieleranzahl vom Server");
-    res.on('data', function(chunk){
-        data = chunk;
-    });
-  });
-  postSpielfigur.end();*/
   request('http://localhost:3000/spielfigur', function (error, response, body) {
     if (!error && response.statusCode == 200) {
       io.emit("players",{c:body});
@@ -117,22 +89,9 @@ function getPlayerCount(){
 }
 
 function abortGame(){
-  /*
-  optionsPOST.path = '/spielfigur';
-
-  var postSpielfigur = server.request(optionsPOST, function(res){
-    console.log("Spieler hat sich verbunden");
-    res.on('data', function(chunk){
-        data = chunk;
-    });
-  });
-  postSpielfigur.end();
-  */
-  data = false;
   request.delete('http://localhost:3000/spielfigur', function (error, response, body) {
     if (!error && response.statusCode == 200) {
-      console.log(body);
+      console.log("Game aborted");
     }
   });
-  return data;
 }
